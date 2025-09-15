32c136c923850678761404e0afcdb78ce55798e3
Akhilesh Verma <152074281+CodePandaAkhilesh@users.noreply.github.com>
2025-09-15 11:15:42 +0530

    Update db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const mongo_url = process.env.MONGODB_URL;
 
-mongoose.connect(mongo_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB Connected...');
-}).catch((err) => {
-    console.log('MongoDB Connection Error: ', err);
-});
+mongoose.connect(mongo_url)
+  .then(() => {
+    console.log('✅ MongoDB Connected...');
+  })
+  .catch((err) => {
+    console.error('❌ MongoDB Connection Error:', err);
+  });
